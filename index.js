a319a5536bfd83c348ad2720bc0d36a42c6a2851
Amiory <amiory05@gmail.com>
2025-09-07 08:24:56 +0330

    Update: - added basic error handling

--- index.js
+++ index.js
@@ -18,16 +18,37 @@ inquirer
   .then((answers) => {
     const pokemon = answers.pokemon;
     const moveCount = answers.moveCount;
-    printMoves(pokemon, moveCount);
+    if (moveCount.match(/\[0-9]+/) || moveCount == "")
+      printMoves(pokemon, moveCount);
+    else console.log("Please Enter a valid number or press Enter");
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("Something went wrong, please try again");
+    } else {
+      const message =
+        error.name === "ExitPromptError"
+          ? "Exiting program"
+          : "Something went wrong, please try again";
+
+      console.log(message);
+    }
   });
 
 const printMoves = async (pokemonName, moveCount) => {
   const response = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
   );
-  const pokemon = await response.json();
-  const moves = pokemon.moves.map(({ move }) => move.name);
-  for (const move of moves.slice(0, moveCount || moves.length)) {
-    console.log(move);
+
+  if (response.ok) {
+    const pokemon = await response.json();
+    console.log(`Printing moves for ${pokemon.name}:`);
+    
+    const moves = pokemon.moves.map(({ move }) => move.name);
+    for (const move of moves.slice(0, moveCount || moves.length)) {
+      console.log(move);
+    }
+  } else if (response.status === 404) {
+    console.log("Pokemon not found");
   }
 };
