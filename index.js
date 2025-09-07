f97cdf35304f5d095085b615d252bc7fb8ff971c
Amiory <amiory05@gmail.com>
2025-09-07 07:53:18 +0330

    Update: - added basic functionality to the cli

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log("amiory-pokedex");
+import yargs from "yargs";
+
+const { argv } = yargs(process.argv);
+
+const printMoves = async (pokemonName, count) => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+  );
+  const pokemon = await response.json();
+  const moves = pokemon.moves.map(({ move }) => move.name);
+  console.log(moves.slice(0, count || 5));
+};
+
+printMoves(argv.pokemon, argv.count);
