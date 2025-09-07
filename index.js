eea525b7459fd30097625f41d0c28853791e8341
Amiory <amiory05@gmail.com>
2025-09-07 08:09:49 +0330

    Update: - added interactivity to the cli using the inquirer package

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 #!/usr/bin/env node
 
-import yargs from "yargs";
+import inquirer from "inquirer";
 
-const { argv } = yargs(process.argv);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "pokemon",
+      message: "What Pokemon's moves do you want to see?",
+    },
+    {
+      type: "input",
+      name: "moveCount",
+      message: "How many moves do you wish do see? (Enter for All)",
+    },
+  ])
+  .then((answers) => {
+    const pokemon = answers.pokemon;
+    const moveCount = answers.moveCount;
+    printMoves(pokemon, moveCount);
+  });
 
-const printMoves = async (pokemonName, count) => {
+const printMoves = async (pokemonName, moveCount) => {
   const response = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
   );
   const pokemon = await response.json();
   const moves = pokemon.moves.map(({ move }) => move.name);
-  console.log(moves.slice(0, count || 5));
+  for (const move of moves.slice(0, moveCount || moves.length)) {
+    console.log(move);
+  }
 };
-
-printMoves(argv.pokemon, argv.count);
